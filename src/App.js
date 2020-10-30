b69eebf957329519c641fa8cbd3e56baa62df95e
GopalDas123 <Gopaldax@gmail.com>
2020-10-30 19:06:35 +0500

    Fetched Data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>hello</h1>
-            </div>
-        )
-    }
+import React from "react";
+import { fetchData } from "./api";
+import styles from "./App.module.css";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    // console.log(data);
+  }
+  render() {
+      const {data}=this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
